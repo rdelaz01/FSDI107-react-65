473996e64eb217e2c9537a8ca5ffba4677ecbdc0
rdelaz01 <rolliejohn.delacruz@gmail.com>
2026-03-10 20:40:11 -0700

    feat: initialize my-react-store project with Vite and React
    - Add package.json with dependencies and scripts for development, build, linting, and preview.
    - Include Vite logo SVG in the public directory.
    - Create App.css for styling the main application layout and components.
    - Implement App.jsx as the main application component with a Navbar and a greeting.
    - Add React logo SVG in the assets directory.
    - Create Navbar component with basic structure and styling.
    - Add global styles in index.css for consistent styling across the application.
    - Set up main entry point in main.jsx to render the App component.
    - Configure Vite with a basic vite.config.js file.

---
+++ my-react-store/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <>
+      <div>
+      <Navbar />
+      <h1>Hello from react</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ my-react-store/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+//import 
+import "./Navbar.css"
+
+//logic
+function Navbar() {
+    return(
+        <div>
+            <h1>My Online Store</h1>
+        </div>
+    )
+}
+
+//export
+export default Navbar; 
