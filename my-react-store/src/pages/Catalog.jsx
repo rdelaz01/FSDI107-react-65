e34f4e1aa3acebd097306a3f4a2c6493d4d4b088
rdelaz01 <rolliejohn.delacruz@gmail.com>
2026-03-14 14:35:39 -0700

    feat: add Bootstrap for styling and implement product filtering in Catalog

--- my-react-store/src/pages/Catalog.jsx
+++ my-react-store/src/pages/Catalog.jsx
@@ -5,6 +5,33 @@ import{useEffect, useState} from "react";
 
 function Catalog() {
     const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState([]); // to hold the categories data service will provide
+    const [productsToDisplay, setProductsToDisplay] = useState([]); // to hold the products that we want to display (filtered by category)
+
+    function loadCatalog(){
+        let service = new DataService();
+        let data = service.getProducts();
+        setProductsToDisplay(data); //initially we want to display all the products, so we set the productsToDisplay to be the same as the products
+        let cats = ["fruit", "berry"]; // we can get the categories from the products data, but for simplicity we will hardcode them here
+        setCategories(cats);
+    }
+
+    function filter(category)
+    {
+        let list =[];
+        //find the products that belong to the category that was clicked
+        for(let i = 0; i < products.length; i++)
+        {
+            let prod = products[i];
+            if(prod.category === category)
+                {
+                    list.push(prod);
+                }
+        }
+        setProductsToDisplay(list);
+
+    }
+
     // const products = [];
     useEffect(()=>
         {
@@ -12,12 +39,15 @@ function Catalog() {
             let service = new DataService();
             let data = service.getProducts();
             setProducts(data);
+            loadCatalog();
 
     },[]); // the empty array mean this effect runs only once 
     return(
         <div className="catalog">
             <h1>Check out our amazing products!</h1>
-            {products.map(prod => <Product key={prod._id} data={prod} />)}
+            {categories.map(cat => <button key={cat} onClick={() => filter(cat)}>{cat}</button>)}
+            <br />
+            {productsToDisplay.map(prod => <Product key={prod._id} data={prod} />)}
         </div>
     )
     
